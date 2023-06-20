0c9011cf07cb08497cbd6c82d9ba8d78d96a1905
Yonatan Cervantes <yonatancervantes539@gmail.com>
2023-06-20 14:43:08 -0600

    footer modificado

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -6,6 +6,8 @@ function Footer() {
         <div className="contenedor contenido">
             <Navegacion />
             <p className="copyright">Todos los derechos reservados {new Date().getFullYear()}</p>
+            <p>Teléfono: 55-64-87-69-56</p>
+            <p>Correo: guitarla@gmail.com</p>
         </div>
     </footer>
   )
